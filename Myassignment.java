9ef0035aac46012b4119f51ec87b5cf568e3298e
Kulubyatracy2002 <2300723593@mubs.ac.ug>
2024-10-04 17:10:30 +0300

    Add files via upload

---
+++ Myassignment.java
@@ -0,0 +1,27 @@
+package myassignment;
+
+public class Myassignment {
+
+	public static void main(String[] args) {
+
+		for(int i=2; i<=6; i++ );
+		System.out.println();
+		
+		int count = 1;
+		do {
+			System.out.println("count:"+ count );
+			count++;
+		} while(count<=5);
+		
+		int w=2;
+		while(w<=6);{
+			System.out.println(w);
+			w++;
+		}
+	}
+	
+	
+	
+	
+
+}
